a4d85be9f7f7d5b23ebfac099653d07a14eea907
Михаил Евпатов <mihailevpatov@MacBook-Pro-Mihail.local>
2022-09-30 17:21:00 +0300

    update

--- async-udp-server/main.cpp
+++ async-udp-server/main.cpp
@@ -1,13 +1,8 @@
-#include <boost/asio.hpp>
-#include <boost/asio/ip/tcp.hpp>
 #include <boost/bind/bind.hpp>
-#include <boost/date_time/posix_time/posix_time_duration.hpp>
-#include <boost/date_time/posix_time/posix_time_types.hpp>
-#include <boost/date_time/posix_time/ptime.hpp>
+#include <boost/asio.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
 #include <boost/core/noncopyable.hpp>
-#include <boost/system/detail/error_code.hpp>
 
 #include <iostream>
 
@@ -15,189 +10,162 @@ using namespace boost::asio;
 using namespace boost::posix_time;
 using namespace boost::placeholders;
 
-boost::asio::io_service service;
+io_service service;
 
 class talk_to_client;
-
 typedef boost::shared_ptr<talk_to_client> client_ptr;
+typedef std::vector<client_ptr> array;
+array clients;
 
-std::vector<client_ptr> clients_ptr_array;
+#define MEM_FN(x)       boost::bind(&self_type::x, shared_from_this())
+#define MEM_FN1(x,y)    boost::bind(&self_type::x, shared_from_this(),y)
+#define MEM_FN2(x,y,z)  boost::bind(&self_type::x, shared_from_this(),y,z)
+
 
 void update_clients_changed();
 
+/** simple connection to server:
+    - logs in just with username (no password)
+    - all connections are initiated by the client: client asks, server answers
+    - server disconnects any client that hasn't pinged for 5 seconds
+    Possible client requests:
+    - gets a list of all connected clients
+    - ping: the server answers either with "ping ok" or "ping client_list_changed"
+*/
 class talk_to_client : public boost::enable_shared_from_this<talk_to_client>
-					, boost::noncopyable {
-	typedef talk_to_client self_type;
-	
-	talk_to_client() : sock_(service), started_(false),
-			timer_(service), clients_changed_(false) {}
-
+        , boost::noncopyable {
+    typedef talk_to_client self_type;
+    talk_to_client() : sock_(service), started_(false),
+                       timer_(service), clients_changed_(false) {
+    }
 public:
-	typedef boost::system::error_code error_code;
-	typedef boost::shared_ptr<talk_to_client> ptr;
+    typedef boost::system::error_code error_code;
+    typedef boost::shared_ptr<talk_to_client> ptr;
 
-	void start() {
-		started_ = true;
-		clients_ptr_array.push_back(shared_from_this());
-		last_ping = boost::posix_time::microsec_clock::local_time();
+    void start() {
+        started_ = true;
+        clients.push_back( shared_from_this());
+        last_ping = boost::posix_time::microsec_clock::local_time();
+        // first, we wait for client to login
+        do_read();
+    }
+    static ptr new_() {
+        ptr new_(new talk_to_client);
+        return new_;
+    }
+    void stop() {
+        if ( !started_) return;
+        started_ = false;
+        sock_.close();
 
-		do_read();
-	}
-
-	static ptr new_() {
-		ptr new_(new talk_to_client);
-		return new_;
-	}
-	
-	void stop() {
-		if (!started_) return;
-		started_ = false;
-		sock_.close();
-
-		ptr self = shared_from_this();
-		std::vector<client_ptr>::iterator it =
-			std::find(clients_ptr_array.begin(), clients_ptr_array.end(), self);
-		
-		clients_ptr_array.erase(it);
-		update_clients_changed();	
-	}
-
-	bool started() const { return started_; }
-	ip::tcp::socket & sock() { return sock_; }
-	std::string username() const { return username_; }
-	void set_clients_changed() { clients_changed_ = true; }
-
+        ptr self = shared_from_this();
+        array::iterator it = std::find(clients.begin(), clients.end(), self);
+        clients.erase(it);
+        update_clients_changed();
+    }
+    bool started() const { return started_; }
+    ip::tcp::socket & sock() { return sock_;}
+    std::string username() const { return username_; }
+    void set_clients_changed() { clients_changed_ = true; }
 private:
-	void on_read(const error_code& err, size_t bytes) {
-		if (err) stop();
-		if (!started()) return;
-		// proc the msg
-		std::string msg(read_buff_, bytes);
-		
-		if (msg.find("login ") == 0) on_login(msg);
-		else if (msg.find("ping") == 0) on_ping();
-		else if (msg.find("ask_clients") == 0) on_clients();
-		else std::cerr << "invalid msg " << msg << std::endl;	
-	}
+    void on_read(const error_code & err, size_t bytes) {
+        if ( err) stop();
+        if ( !started() ) return;
+        // process the msg
+        std::string msg(read_buffer_, bytes);
+        if ( msg.find("login ") == 0) on_login(msg);
+        else if ( msg.find("ping") == 0) on_ping();
+        else if ( msg.find("ask_clients") == 0) on_clients();
+        else std::cerr << "invalid msg " << msg << std::endl;
+    }
 
-	void on_login(const std::string & msg) {
-		std::istringstream in(msg);
-		in >> username_ >> username_;
-		std::cout << "username_" << " logged in" << std::endl;
-		do_write("login ok\n");		
-		update_clients_changed();
-	}
+    void on_login(const std::string & msg) {
+        std::istringstream in(msg);
+        in >> username_ >> username_;
+        std::cout << username_ << " logged in" << std::endl;
+        do_write("login ok\n");
+        update_clients_changed();
+    }
+    void on_ping() {
+        do_write(clients_changed_ ? "ping client_list_changed\n" : "ping ok\n");
+        clients_changed_ = false;
+    }
+    void on_clients() {
+        std::string msg;
+        for( array::const_iterator b = clients.begin(), e = clients.end() ; b != e; ++b)
+            msg += (*b)->username() + " ";
+        do_write("clients " + msg + "\n");
+    }
 
-	void on_ping() {
-		do_write(clients_changed_ ? "ping clients_list_changed\n" : "ping ok\n");
-		clients_changed_ = false;
-	}
+    void do_ping() {
+        do_write("ping\n");
+    }
+    void do_ask_clients() {
+        do_write("ask_clients\n");
+    }
 
-	void on_clients() {
-		std::string msg;
+    void on_check_ping() {
+        boost::posix_time::ptime now = boost::posix_time::microsec_clock::local_time();
+        if ( (now - last_ping).total_milliseconds() > 5000) {
+            std::cout << "stopping " << username_ << " - no ping in time" << std::endl;
+            stop();
+        }
+        last_ping = boost::posix_time::microsec_clock::local_time();
+    }
+    void post_check_ping() {
+        timer_.expires_from_now(boost::posix_time::millisec(5000));
+        timer_.async_wait( MEM_FN(on_check_ping));
+    }
 
-		for (
-			std::vector<client_ptr>::const_iterator b =
-				clients_ptr_array.begin(),
-			e = clients_ptr_array.end();
-			b != e; ++b
-		) {
-			msg += (*b)->username() + " ";
-		}
-	
-		do_write("clients " + msg + "\n");
-	}
 
-	void do_ping() {
-		do_write("ping\n");
-	}
-
-	void do_ask_clients() {
-		do_write("ask_clients\n");
-	}
-
-	void on_check_ping() {
-		boost::posix_time::ptime now =
-			boost::posix_time::microsec_clock::local_time();
-		
-		if ((now - last_ping).total_milliseconds() > 5000) {
-			std::cout << "stopping " << username_ <<
-						 " - no ping in time" << std::endl;
-			stop();	
-		}
-		last_ping = boost::posix_time::microsec_clock::local_time();
-	}
-
-	void post_check_ping() {
-		timer_.expires_from_now(boost::posix_time::millisec(5000));
-		timer_.async_wait(boost::bind(&talk_to_client::on_check_ping,
-									  shared_from_this()));
-
-	}
-
-	void on_write(const error_code & err, size_t bytes) {
-		do_read();
-	}	
-
-	void do_read() {
-		async_read(sock_,
-				   buffer(read_buff_),
-				   boost::bind(&talk_to_client::read_compltete,
-							   shared_from_this(), _1, _2),
-				   boost::bind(&talk_to_client::on_read,
-							   shared_from_this(),_1, _2));
-		post_check_ping();
-	}
-
-	void do_write(const std::string & msg) {
-		if (!started()) return;
-		std::copy(msg.begin(), msg.end(), write_buff_);
-		sock_.async_write_some(buffer(write_buff_, msg.size()),
-									  boost::bind(&talk_to_client::on_write,
-												  shared_from_this(), _1, _2));
-	}
-
-	size_t read_compltete(
-		const boost::system::error_code & err,
-		size_t bytes
-	) {
-		if (err) return 0;
-		bool found = std::find(read_buff_, read_buff_ + bytes, '\n')
-			< read_buff_ + bytes;
-		return found ? 0 : 1;
-	}
-
+    void on_write(const error_code & err, size_t bytes) {
+        do_read();
+    }
+    void do_read() {
+        async_read(sock_, buffer(read_buffer_),
+                   MEM_FN2(read_complete,_1,_2), MEM_FN2(on_read,_1,_2));
+        post_check_ping();
+    }
+    void do_write(const std::string & msg) {
+        if ( !started() ) return;
+        std::copy(msg.begin(), msg.end(), write_buffer_);
+        sock_.async_write_some( buffer(write_buffer_, msg.size()),
+                                MEM_FN2(on_write,_1,_2));
+    }
+    size_t read_complete(const boost::system::error_code & err, size_t bytes) {
+        if ( err) return 0;
+        bool found = std::find(read_buffer_, read_buffer_ + bytes, '\n') < read_buffer_ + bytes;
+        // we read one-by-one until we get to enter, no buffering
+        return found ? 0 : 1;
+    }
 private:
-	ip::tcp::socket sock_;
-	enum { max_msg = 1024 };
-	char read_buff_[max_msg];
-	char write_buff_[max_msg];
-	bool started_;
-	std::string username_;
-	deadline_timer timer_;
-	boost::posix_time::ptime last_ping;
-	bool clients_changed_;
+    ip::tcp::socket sock_;
+    enum { max_msg = 1024 };
+    char read_buffer_[max_msg];
+    char write_buffer_[max_msg];
+    bool started_;
+    std::string username_;
+    deadline_timer timer_;
+    boost::posix_time::ptime last_ping;
+    bool clients_changed_;
 };
 
 void update_clients_changed() {
-	for (std::vector<client_ptr>::iterator b = clients_ptr_array.begin(),
-		 e = clients_ptr_array.end(); b != e; ++b)
-		(*b)->set_clients_changed();
+    for( array::iterator b = clients.begin(), e = clients.end(); b != e; ++b)
+        (*b)->set_clients_changed();
 }
 
-ip::tcp::acceptor acceptor(service,
-						   ip::tcp::endpoint(ip::tcp::v4(), 8001));
+ip::tcp::acceptor acceptor(service, ip::tcp::endpoint(ip::tcp::v4(), 8001));
 
-void handle_accept(talk_to_client::ptr client,
-				   const boost::system::error_code & err) {
-	client->start();
-	talk_to_client::ptr new_client = talk_to_client::new_();
-	acceptor.async_accept(new_client->sock(),
-						  boost::bind(handle_accept, new_client, _1));
+void handle_accept(talk_to_client::ptr client, const boost::system::error_code & err) {
+    client->start();
+    talk_to_client::ptr new_client = talk_to_client::new_();
+    acceptor.async_accept(new_client->sock(), boost::bind(handle_accept,new_client,_1));
 }
 
-int main(void) {
-	talk_to_client::ptr client = talk_to_client::new_();
-	acceptor.async_accept(client->sock(), boost::bind(handle_accept, client, _1));
-	service.run();
+
+int main(int argc, char* argv[]) {
+    talk_to_client::ptr client = talk_to_client::new_();
+    acceptor.async_accept(client->sock(), boost::bind(handle_accept,client,_1));
+    service.run();
 }
